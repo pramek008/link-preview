4d2be283ecac82fe485331d0cf37142617f55ccc
pramek008 <pramudyaeka.dev@gmail.com>
2025-07-27 23:05:42 +0700

    update platform config for tiktok

--- config/platform_config.py
+++ config/platform_config.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from typing import List, Dict
-
 @dataclass
 class PlatformSelectors:
     title: List[str]
@@ -12,47 +11,55 @@ class PlatformConfig:
         self.selectors = selectors
 
 PLATFORM_CONFIGS: Dict[str, PlatformConfig] = {
-    "default": PlatformConfig(
-        PlatformSelectors(
-            title=['meta[property="og:title"]', 'meta[name="twitter:title"]', 'title'],
-            description=['meta[property="og:description"]', 'meta[name="twitter:description"]', 'meta[name="description"]'],
-            image=['meta[property="og:image"]', 'meta[name="twitter:image"]']
-        )
-    ),
-    "twitter.com": PlatformConfig(
-        PlatformSelectors(
-            title=['[data-testid="tweetText"]', 'meta[property="og:title"]'],
-            description=['[data-testid="tweetText"]', 'meta[property="og:description"]'],
-            image=['[data-testid="tweetPhoto"] img', 'meta[property="og:image"]']
-        )
-    ),
-    "facebook.com": PlatformConfig(
-        PlatformSelectors(
-            title=['meta[property="og:title"]', 'meta[name="twitter:title"]'],
-            description=['meta[property="og:description"]', 'meta[name="twitter:description"]'],
-            image=['meta[property="og:image"]', 'meta[name="twitter:image"]'],
-        )
-    ),
-    "youtube.com": PlatformConfig(
-        PlatformSelectors(
-            title=['meta[property="og:title"]', 'meta[name="twitter:title"]'],
-            description=['meta[property="og:description"]', 'meta[name="twitter:description"]'],
-            image=['meta[property="og:image"]', 'meta[name="twitter:image"]'],
-        )
-    ),
-    "linkedin.com": PlatformConfig(
-        PlatformSelectors(
-            title=['meta[property="og:title"]', 'meta[name="twitter:title"]'],
-            description=['meta[property="og:description"]', 'meta[name="twitter:description"]'],
-            image=['meta[property="og:image"]', 'meta[name="twitter:image"]'],
-        )
-    ),
-    "twitter.com": PlatformConfig(
-        PlatformSelectors(
-            title=['meta[property="og:title"]', 'meta[name="twitter:title"]'],
-            description=['meta[property="og:description"]', 'meta[name="twitter:description"]'],
-            image=['meta[property="og:image"]', 'meta[name="twitter:image"]'],
-        )
-    ),
-    # ... other platform configs remain the same
+    "default": PlatformConfig(PlatformSelectors(
+        title=['meta[property="og:title"]', 'meta[name="twitter:title"]', 'title'],
+        description=['meta[property="og:description"]', 'meta[name="twitter:description"]', 'meta[name="description"]'],
+        image=['meta[property="og:image"]', 'meta[name="twitter:image"]']
+    )),
+    "instagram.com": PlatformConfig(PlatformSelectors(
+        title=['meta[property="og:title"]', 'meta[name="twitter:title"]', 'title'],
+        description=['meta[property="og:description"]', 'meta[name="twitter:description"]', 'meta[name="description"]'],
+        image=['meta[property="og:image"]', 'meta[name="twitter:image"]']
+    )),
+    "twitter.com": PlatformConfig(PlatformSelectors(
+        title=['[data-testid="tweetText"]', 'meta[property="og:title"]'],
+        description=['[data-testid="tweetText"]', 'meta[property="og:description"]'],
+        image=['[data-testid="tweetPhoto"] img', 'meta[property="og:image"]']
+    )),
+    "tiktok.com": PlatformConfig(PlatformSelectors(
+        title=[
+            'title'
+            'meta[property="title"]',
+            'meta[property="og:title"]',
+            '[data-e2e="video-desc"]', 
+            '[data-e2e="browse-video-desc"]',
+        ],
+        description=[
+            'meta[name="description"]'
+            'meta[property="description"]',
+            'meta[property="og:description"]',
+            '[data-e2e="video-desc"]',
+            '[data-e2e="browse-video-desc"]',
+        ],
+        image=[
+            'meta[property="og:image"]',
+            'meta[name="twitter:image"]'
+            '[data-e2e="browse-video-cover"] img',
+        ]
+    )),
+    "facebook.com": PlatformConfig(PlatformSelectors(
+        title=['meta[property="og:title"]', 'meta[name="twitter:title"]'],
+        description=['meta[property="og:description"]', 'meta[name="twitter:description"]'],
+        image=['meta[property="og:image"]', 'meta[name="twitter:image"]']
+    )),
+    "youtube.com": PlatformConfig(PlatformSelectors(
+        title=['meta[property="og:title"]', 'meta[name="twitter:title"]'],
+        description=['meta[property="og:description"]', 'meta[name="twitter:description"]'],
+        image=['meta[property="og:image"]', 'meta[name="twitter:image"]']
+    )),
+    "linkedin.com": PlatformConfig(PlatformSelectors(
+        title=['meta[property="og:title"]', 'meta[name="twitter:title"]'],
+        description=['meta[property="og:description"]', 'meta[name="twitter:description"]'],
+        image=['meta[property="og:image"]', 'meta[name="twitter:image"]']
+    ))
 }
